15b2cacc70819ab897654ab0eb16e0fce6007467
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-09-05 01:01:59 -0700

    Update wiki_to_netflix.py

--- src/data_processing/wiki_to_netflix.py
+++ src/data_processing/wiki_to_netflix.py
@@ -2,74 +2,112 @@ import pandas as pd
 import zipfile
 from pprint import pprint
 import requests
+import csv
 
+#Reading netflix text file
+def read_netflix_txt(txt_file):
+    with open(txt_file, "r", encoding = "ISO-8859-1") as netflix_data:
+        netflix_list = [line.rstrip().split(',', 2) for line in netflix_data.readlines()]
+    return(netflix_list)
 
-#Reading in dataset
-netflix_df = pd.read_csv("movie_titles.txt", header = None, encoding = "ISO-8859-1", delimiter = ",", on_bad_lines='skip')
-netflix_df.columns = ["ID", "Year", "Title"] # renaming titles
-netflix_df.Year = netflix_df.Year.astype("Int64") # removing decimal from year
-sub_netflix_df = netflix_df.head(100) #working with subset of dataset for now
+#Writing netflix csv file
+def create_netflix_csv(csv_name, data_list):   
+    with open(csv_name, 'w') as netflix_csv:
+        csv.writer(netflix_csv).writerows(data_list)
 
-#Extracting movie feature info from request
-def wiki_feature_info(data, key):
+#Extracting movie info from Wiki data
+def Wiki_feature_info(data, key):
     if len(data['results']['bindings']) < 1 or key not in data['results']['bindings'][0]:
         return 'NA'
     else:
-        return data['results']['bindings'][0][key]['value'].split('/')[-1]
+        return(data['results']['bindings'][0][key]['value'].split('/')[-1])
 
+#Adding movie info to netflix csv file
+def add_movie_info_to_csv(data_csv, movies, genres, directors):
+    with open(data_csv, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        data_list = list(csv_reader)
+        
+    for i in range(1, len(data_list)):
+        if movies[i] and genres[i] and directors[i] == "NA":
+            pass
+        else:
+            data_list[i].extend((movies[i], genres[i], directors[i]))
+            
+    with open(data_csv, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerows(data_list)  
 
 #Getting list of movie IDs, genre IDs, and director IDs from request
-def Wiki_query(dataset):
+def Wiki_query(data_csv, user_agent):
     wiki_movie_ids = []
     wiki_genres = []
     wiki_directors = []
-    for index, row in dataset.iterrows():
-        SPARQL = '''
-        SELECT * WHERE {
-          SERVICE wikibase:mwapi {
-            bd:serviceParam wikibase:api "EntitySearch" ;
-                            wikibase:endpoint "www.wikidata.org" ;
-                            mwapi:search "%s" ;
-                            wikibase:limit 1 ;
-                            mwapi:language "en" .
-            ?item wikibase:apiOutputItem mwapi:item .
+    
+    csv_reader = csv.reader(open(data_csv))
+    
+    for row in csv_reader:
+        if row[1] != "NULL":
+            SPARQL = '''
+            SELECT * WHERE {
+              SERVICE wikibase:mwapi {
+                  bd:serviceParam wikibase:endpoint "www.wikidata.org";
+                      wikibase:api "EntitySearch";
+                      mwapi:search "%(Title)s";
+                      mwapi:language "en".
+              ?item wikibase:apiOutputItem mwapi:item.
           }
-          {
-            ?item wdt:P31/wdt:P279* wd:Q11424 ;
-                  wdt:P577 ?releaseDate ;
-                  rdfs:label ?itemLabel ;
-                  OPTIONAL {?item wdt:P136 ?genreLabel.}
-                  OPTIONAL {?item wdt:P57 ?directorLabel.}
-            FILTER("%d-01-01"^^xsd:dateTime <= ?releaseDate && ?releaseDate < "%d-05-01"^^xsd:dateTime) .
-            FILTER (lang(?itemLabel) = "en") .
-            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . } 
-          }
-        }
-        LIMIT 1
-        ''' % (row["Title"], row["Year"], row["Year"])
+              ?item wdt:P31/wdt:P279* wd:Q11424 ;
+                  wdt:P577 ?releaseDate .
+                  SERVICE wikibase:label {
+                    ?item rdfs:label ?itemLabel .
+                    bd:serviceParam wikibase:language "en". 
+                  }
+                  
+                  OPTIONAL {?item wdt:P136 ?genre. 
+                                      ?genre rdfs:label ?genreLabel.
+                                      FILTER (lang(?genreLabel) = "en")}
+                OPTIONAL {?item wdt:P57 ?director.
+                                      ?director rdfs:label ?directorLabel.
+                                      FILTER (lang(?directorLabel) = "en")}
+                  
+                  FILTER("%(Year)d-01-01"^^xsd:dateTime <= ?releaseDate && ?releaseDate < "%(Next_year)d-01-01"^^xsd:dateTime) .
+                } LIMIT 1
+            
+        
+            ''' % {'Title': row[2], 'Year': int(row[1]), 'Next_year': int(row[1]) + 1}
 
-        response = requests.post('https://query.wikidata.org/sparql',
-                      headers={'User-Agent': 'Noisebridge MovieBot 0.0.1/smaysen <smaysen@gmail.com>'},
+            response = requests.post('https://query.wikidata.org/sparql',
+                      headers={'User-Agent': user_agent},
                       data={
                         'query': SPARQL,
                         'format': 'json',
                       }
         )
-        response.raise_for_status()
-
-        data = response.json()        
-
-        wiki_movie_ids.append(wiki_feature_info('item'))
-        wiki_genres.append(wiki_feature_info('genreLabel'))
-        wiki_directors.append(wiki_feature_info('directorLabel')
-
+            response.raise_for_status() 
             
-    return(wiki_movie_ids,wiki_genres, wiki_directors)
+            data = response.json()
+            
+            wiki_movie_ids.append(Wiki_feature_info(data, 'item'))
+            wiki_genres.append(Wiki_feature_info(data, 'genreLabel'))
+            wiki_directors.append(Wiki_feature_info(data, 'directorLabel'))
+      
+        else:
+            pass
+     
+    return(wiki_movie_ids, wiki_genres, wiki_directors)
 
-#Calling function
-wiki_movie_ids_list, wiki_genres_list, wiki_directors_list = Wiki_query(sub_netflix_df)
 
-#Adding movie, genres, and director ids to dataframe
-sub_netflix_df["Movie_IDs"] = wiki_movie_ids_list
-sub_netflix_df["Genre_IDs"] = wiki_genres_list
-sub_netflix_df["Director_IDs"] = wiki_directors_list
+
+#Calling all functions
+
+from gitignore import netflix_txt, user_agent
+
+netflix_file = read_netflix_txt(netflix_txt)
+
+netflix_csv = 'netflix_movies.csv'
+create_netflix_csv(netflix_csv, netflix_file)
+
+wiki_movie_ids_list, wiki_directors_list, wiki_genres_list = Wiki_query(netflix_csv, user_agent)
+
+add_movie_info_to_csv(netflix_csv, wiki_movie_ids_list, wiki_genres_list, wiki_directors_list)
