08099b62e51be92b14c04e7ecb9faf4b1b43eb39
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-09-30 23:05:50 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-# Main entry point to run the recommendation pipeline
+from data_processing.wiki_to_netflix import * 
+
+if __name__ == '__main__':
+    data = process_data(True)
+    insert_into_mongo(data)
