881f410f8c6c281e121f72d942a0f319cc6183e6
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-10-02 22:23:38 -0700

    Update src/data_processing/wiki_to_netflix.py
    Co-authored-by: Travis Briggs <audiodude@gmail.com>

--- src/data_processing/wiki_to_netflix.py
+++ src/data_processing/wiki_to_netflix.py
@@ -133,9 +133,9 @@ def process_data(test=False):
 
     create_netflix_csv(netflix_csv, processed_data)
 
-    print('missing:', missing_count, '(', missing_count / num_rows * 100, '%)')
-    print('found:', num_rows - missing_count, '(', (num_rows - missing_count) / num_rows * 100, '%)')
-    print('total:', num_rows)
+    print(f'missing:  {missing_count} ({missing_count / num_rows * 100}%)')
+    print(f'found: {num_rows - missing_count} ({num_rows - missing_count) / num_rows * 100}%)')
+    print(f'total: {num_rows}')
 
 if __name__ == '__main__':
     process_data(True)
