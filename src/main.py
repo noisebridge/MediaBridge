82bc4ca4a169dd0a4ec2c6a8fba4fd3ec4c092f1
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-10-09 23:40:46 -0700

    Removing logging

--- src/main.py
+++ src/main.py
@@ -2,15 +2,7 @@
 
 from data_processing.wiki_to_netflix import * 
 
-import logging
-
-logger = logging.getLogger(__name__)
-
 if __name__ == '__main__':
     test = len(sys.argv) < 2 or sys.argv[1] != '--prod'
-    logging.basicConfig(filename='program_log.log', level=logging.INFO)
-    logger.info('Program running starts')
     data = process_data(test = test)
-    logger.info('Program successfully processes Netflix data')
     insert_into_mongo(data)
-    logger.info('Program successfully stores data')
