6bb1dd66fb64fdeb65bb7d2e9b1bd36e10ddfdc8
corriestoddard <corriestoddard@gmail.com>
2024-10-16 18:43:22 -0700

    Add test for format_sparql_query

---
+++ tests/constants.py
@@ -0,0 +1,45 @@
+SPARQL_QUERY ='''
+        SELECT * WHERE {
+            SERVICE wikibase:mwapi {
+                bd:serviceParam wikibase:api "EntitySearch" ;
+                                wikibase:endpoint "www.wikidata.org" ;
+                                mwapi:search "The Room" ;
+                                mwapi:language "en" .
+                ?item wikibase:apiOutputItem mwapi:item .
+            }
+
+            ?item wdt:P31/wdt:P279* wd:Q11424 .
+            
+            {
+                # Get US release date
+                ?item p:P577 ?releaseDateStatement .
+                ?releaseDateStatement ps:P577 ?releaseDate .
+                ?releaseDateStatement pq:P291 wd:Q30 .  
+            }
+            UNION
+            {
+                # Get unspecified release date
+                ?item p:P577 ?releaseDateStatement .
+                ?releaseDateStatement ps:P577 ?releaseDate .
+                FILTER NOT EXISTS { ?releaseDateStatement pq:P291 ?country }
+            }
+        
+            FILTER (YEAR(?releaseDate) = 2003) .
+
+            ?item rdfs:label ?itemLabel .
+            FILTER (lang(?itemLabel) = "en") .
+
+            OPTIONAL {
+                ?item wdt:P136 ?genre .
+                ?genre rdfs:label ?genreLabel .
+                FILTER (lang(?genreLabel) = "en") .
+            }
+
+            OPTIONAL {?item wdt:P57 ?director.
+                            ?director rdfs:label ?directorLabel.
+                            FILTER (lang(?directorLabel) = "en")}
+
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
+            }
+    
+        '''
--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,4 +1,6 @@
 from src.data_processing.wiki_to_netflix import format_sparql_query, wiki_query, process_data
+from . import constants
 
-def test_process_data():
-    process_data(test=True)
+def test_format_sparql_query():
+    QUERY = format_sparql_query("The Room", 2003)
+    assert QUERY == constants.SPARQL_QUERY
