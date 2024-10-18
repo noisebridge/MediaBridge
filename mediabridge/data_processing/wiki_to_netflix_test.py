d3442d088971e08bc82164f3ebde7d3be2e02778
corriestoddard <corriestoddard@gmail.com>
2024-10-17 22:03:47 -0700

    Refactor

--- mediabridge/data_processing/wiki_to_netflix_test.py
+++ mediabridge/data_processing/wiki_to_netflix_test.py
@@ -1,5 +1,5 @@
-from src.data_processing.wiki_to_netflix import format_sparql_query, wiki_query, process_data
-from . import constants
+from wiki_to_netflix import format_sparql_query, wiki_query, process_data
+from ...tests import constants
 
 def test_format_sparql_query():
     QUERY = format_sparql_query("The Room", 2003)
