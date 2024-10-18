4398f40abe9c3d6366d6423724912a68747b34e2
corriestoddard <corriestoddard@gmail.com>
2024-10-17 22:09:51 -0700

    Update imports

--- mediabridge/data_processing/wiki_to_netflix_test.py
+++ mediabridge/data_processing/wiki_to_netflix_test.py
@@ -1,6 +1,6 @@
 from wiki_to_netflix import format_sparql_query, wiki_query, process_data
-from ...tests import constants
+from . import wiki_to_netflix_test_data
 
 def test_format_sparql_query():
     QUERY = format_sparql_query("The Room", 2003)
-    assert QUERY == constants.SPARQL_QUERY
+    assert QUERY == wiki_to_netflix_test_data.SPARQL_QUERY
