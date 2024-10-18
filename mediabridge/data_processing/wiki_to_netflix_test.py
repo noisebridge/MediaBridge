afa713da916fca8f88c0429dd8f3baf3e8d5146c
corriestoddard <corriestoddard@gmail.com>
2024-10-17 22:19:51 -0700

    Fix tests

--- mediabridge/data_processing/wiki_to_netflix_test.py
+++ mediabridge/data_processing/wiki_to_netflix_test.py
@@ -1,6 +1,6 @@
 from wiki_to_netflix import format_sparql_query, wiki_query, process_data
-from . import wiki_to_netflix_test_data
+from wiki_to_netflix_test_data import SPARQL_QUERY
 
 def test_format_sparql_query():
     QUERY = format_sparql_query("The Room", 2003)
-    assert QUERY == wiki_to_netflix_test_data.SPARQL_QUERY
+    assert QUERY == SPARQL_QUERY
