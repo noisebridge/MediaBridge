90fe3632d21c43215b40c019843428ff3a6314d9
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-11-04 18:15:56 -0800

    Delete src directory

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from data_processing.wiki_to_netflix import * 
-
-if __name__ == '__main__':
-    test = len(sys.argv) < 2 or sys.argv[1] != '--prod'
-    data = process_data(test = test)
-    insert_into_mongo(data)
