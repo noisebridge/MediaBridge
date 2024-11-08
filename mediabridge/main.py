84dcf2bba119ed385850678e37622469132098e0
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-11-07 17:22:55 -0800

    Adding back in code that had been removed

--- mediabridge/main.py
+++ mediabridge/main.py
@@ -1,5 +1,8 @@
 from data_processing.wiki_to_netflix import * 
 
+q = wiki_to_netflix.format_sparql_query('The Room', 2003)
+print(q)
+
 if __name__ == '__main__':
     test = len(sys.argv) < 2 or sys.argv[1] != '--prod'
     data = process_data(test = test)
