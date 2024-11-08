f7e77094f5911886d3573150e5e10c402c2d533c
Schuyler Fenton <dev@skyfenton.com>
2024-11-08 06:34:16 +0000

    Add --test option for logging a subset of items

--- mediabridge/main.py
+++ mediabridge/main.py
@@ -8,7 +8,19 @@ from mediabridge.data_processing import wiki_to_netflix
 LOG = logging.getLogger(__name__)
 
 
-def main(verbose: bool = typer.Option(False, "--verbose", "-v")):
+def main(verbose: bool = typer.Option(False, "--verbose", "-v"), test: bool = False):
+    if test:
+        # log all messages to file
+        logging.basicConfig(
+            level=logging.DEBUG,
+            filename="mediabridge.log",
+            filemode="a",
+            format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+            datefmt="%H:%M:%S",
+        )
+        wiki_to_netflix.process_data(True)
+        return
+
     if verbose:
         logging.basicConfig(level=logging.INFO)
 
