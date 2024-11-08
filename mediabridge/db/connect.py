f315125ad0c06832eab5da5002a53323bf7d84ca
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-11-07 18:38:15 -0800

    Inserting data into mongo db

--- mediabridge/db/connect.py
+++ mediabridge/db/connect.py
@@ -1,1 +1,27 @@
-# MongoDB connection setup
+from dotenv import load_dotenv
+import os
+import pymongo
+from pymongo import MongoClient
+
+load_dotenv()
+
+def connect_to_mongo():
+    db_username = os.getenv('mongodb_username')
+    db_password = os.getenv('mongodb_password')
+    mongo_url = os.getenv('mongodb_uri')
+    client = MongoClient(mongo_url, username = db_username, password = db_password)
+    db = client['mediabridge']
+    collection = db["movies"]
+    return collection
+
+def insert_into_mongo(movie):
+    get_collection = connect_to_mongo()
+    get_collection.update_one({'wikidata_id': movie[1]}, 
+        {'set': {
+        'netflix_id': movie[0],
+        'wikidata_id': movie[1],
+        'title': movie[2],
+        'year': movie[3],
+        'genre': movie[4],
+        'director': movie[5]} },
+        upsert = True)
