8a66bcfc22282123ce9e08b305ef87f60f846c24
smaysenhalder <168503776+smaysenhalder@users.noreply.github.com>
2024-11-07 18:28:57 -0800

    Commenting this out

--- mediabridge/main.py
+++ mediabridge/main.py
@@ -1,7 +1,7 @@
 from data_processing.wiki_to_netflix import process_data, insert_into_mongo
 
-q = wiki_to_netflix.format_sparql_query('The Room', 2003)
-print(q)
+#q = wiki_to_netflix.format_sparql_query('The Room', 2003)
+#print(q)
 
 if __name__ == '__main__':
     test = len(sys.argv) < 2 or sys.argv[1] != '--prod'
