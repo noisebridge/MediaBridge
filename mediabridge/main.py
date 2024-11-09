fda76fadd64bab43565fb497edb4e83a883b5ade
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-08 20:55:31 -0800

    Somehow we lost "import sys" -- restore it

--- mediabridge/main.py
+++ mediabridge/main.py
@@ -1,5 +1,7 @@
-from mediabridge.db.insert_data import process_data
+import sys
+
 from mediabridge.db.connect import insert_into_mongo
+from mediabridge.db.insert_data import process_data
 
 #q = wiki_to_netflix.format_sparql_query('The Room', 2003)
 #print(q)
