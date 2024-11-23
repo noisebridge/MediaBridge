038ad44028edb48eef2bb821060188cc61113988
corriestoddard <corriestoddard@gmail.com>
2024-11-22 22:07:46 -0800

    Remove extra else statement

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -9,24 +9,25 @@ def create_interaction_matrix(directory_path, num_users, num_movies, files):
     interaction_matrix = np.zeros((num_users, num_movies), dtype=np.int8)
     user_mapper = {}
     current_user_index = 0
-    
+
     for filename in files:
-        with open(os.path.join(directory_path, filename), 'r') as file:
-            movie_id = int(file.readline().strip().replace(':', ''))
+        with open(os.path.join(directory_path, filename), "r") as file:
+            movie_id = int(file.readline().strip().replace(":", ""))
             movie_idx = movie_id - 1
-            
+
             for line in file:
-                user_id, rating, _ = line.strip().split(',')
+                user_id, rating, _ = line.strip().split(",")
                 user_id = int(user_id)
                 rating = int(rating)
-                
+
                 if rating < 4:
                     continue
-                else:
-                    if user_id not in user_mapper:
-                        user_mapper[user_id] = current_user_index
-                        current_user_index += 1
-                    user_idx = user_mapper[user_id]
-                    interaction_matrix[user_idx, movie_idx] = rating
+
+                if user_id not in user_mapper:
+                    user_mapper[user_id] = current_user_index
+                    current_user_index += 1
+
+                user_idx = user_mapper[user_id]
+                interaction_matrix[user_idx, movie_idx] = rating
 
     return interaction_matrix
