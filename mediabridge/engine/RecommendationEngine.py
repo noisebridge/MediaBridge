465090cfa9e8ad74fcb5d43ab25918d1362ce5c6
corriestoddard <corriestoddard@gmail.com>
2024-11-24 12:35:46 -0800

    Refactor

--- mediabridge/engine/RecommendationEngine.py
+++ mediabridge/engine/RecommendationEngine.py
@@ -1,10 +1,12 @@
 import pickle
 
 import numpy as np
+from scipy import sparse
 
 
 class RecommendationEngine:
-    def __init__(self, model_file):
+    def __init__(self, data, model_file):
+        self.data = data
         with open(model_file, "rb") as f:
             self.model = pickle.load(f)
 
@@ -14,10 +16,11 @@ class RecommendationEngine:
     def ids_to_titles(self, ids):
         pass
 
-    def get_recommendations(self, user_id, movie_ids):
+    def get_recommendations(self, user_id, user_interactions):
         scores = self.model.predict(
-            user_id,
-            movie_ids,
+            user_ids=0,
+            item_ids=self.data,
+            user_features=user_interactions,
         )
         return np.argsort(-scores)
 
@@ -31,8 +34,16 @@ class RecommendationEngine:
         for title in movie_titles:
             print(title)
 
+    def create_user_matrix(self, liked_movies_ids):
+        user_interactions = sparse.csr_matrix((1, self.num_items), dtype=np.float32)
+        for movie_id in liked_movies_ids:
+            user_interactions[0, movie_id] = 1
+        return user_interactions
+
     def recommend(self, user_id=1):
         liked_movies = self.get_data(user_id)
         liked_movies_ids = self.titles_to_ids(liked_movies)
-        recommendations = self.get_recommendations(user_id, liked_movies_ids)
-        self.display_recommendations(recommendations)
+        user_interactions = self.create_user_matrix(liked_movies_ids)
+
+        recommendations = self.get_recommendations(user_id, user_interactions)
+        self.display_recommendations(recommendations[:10])
