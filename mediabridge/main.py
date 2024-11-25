80a892e64da1120cb26254c17f8b947adddee423
Schuyler Fenton <dev@skyfenton.com>
2024-11-25 22:02:39 +0000

    Document options and explanations

--- mediabridge/main.py
+++ mediabridge/main.py
@@ -10,11 +10,16 @@ from mediabridge.definitions import OUTPUT_DIR
 
 
 def main(
-    verbose: bool = typer.Option(False, "--verbose", "-v"),
-    log: bool = False,
-    full: bool = False,
+    verbose: bool = typer.Option(
+        False, "--verbose", "-v", help="Enable verbose logging."
+    ),
+    log: bool = typer.Option(
+        False, "--log", "-l", help="Enable all logging message levels and log to file."
+    ),
+    full: bool = typer.Option(
+        False, "--full", "-f", help="Run processing on full dataset."
+    ),
 ):
-    # create output directory if it doesn't exist
     if not OUTPUT_DIR.exists():
         print(
             f"[WARNING] Output directory does not exist, creating new directory at {OUTPUT_DIR}"
@@ -22,7 +27,7 @@ def main(
         OUTPUT_DIR.mkdir()
 
     if log:
-        # log all messages to file
+        # log all messages to new file
         logging.basicConfig(
             level=logging.DEBUG,
             filename=OUTPUT_DIR.joinpath(
@@ -39,13 +44,14 @@ def main(
             level = logging.WARNING
         logging.basicConfig(level=level, format="[%(levelname)s] %(message)s")
 
-    # If writing to stdout (log is false), redirect logging to tqdm.write
-    # function to avoid colliding with progress bar formatting
+    # We redirect logs to stdout through tqdm to avoid breaking progress bar.
+    # But when logging to file, we use nullcontext or tqdm will redirect logs
+    # back to stdout.
     with logging_redirect_tqdm() if not log else nullcontext():
         try:
             wiki_to_netflix.process_data(not full)
         except Exception as e:
-            # include fatal exceptions with traceback in log
+            # include fatal exceptions with traceback in logs
             if log:
                 logging.exception("Uncaught exception")
             raise e
