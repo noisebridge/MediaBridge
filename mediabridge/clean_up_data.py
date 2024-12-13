79e0df5c262fe68642d3c601efc28ffd795e20fe
JamesKohlsRepo <jameskohls@live.com>
2024-12-12 20:31:47 -0800

    Added clean_up_data and download_data
    these are intended to be run as pipenv scripts:
    pipenv run download_data
    pipenv run delete_data
    
    they can also be run from file

---
+++ mediabridge/clean_up_data.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+
+def clean_data_folder(data_dir):
+    """
+    Deletes everything inside the 'data' folder but keeps the 'data' folder itself.
+    """
+    if not os.path.exists(data_dir):
+        print(f"No 'data' folder found at {data_dir}. Nothing to clean.")
+        return
+
+    print(f"Cleaning up all contents of the 'data' folder at: {data_dir}...")
+
+    for item in os.listdir(data_dir):
+        item_path = os.path.join(data_dir, item)
+
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+            print(f"Deleted file: {item_path}")
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+            print(f"Deleted directory: {item_path}")
+
+    print(f"All contents of the 'data' folder have been deleted.")
+
+
+if __name__ == "__main__":
+    data_dir = os.path.join(os.path.dirname(__file__), "../data")
+
+    clean_data_folder(data_dir)
--- mediabridge/download_data.py
+++ mediabridge/download_data.py
@@ -1,4 +1,7 @@
-# file
+import os
+import tarfile
+
+import wget
 
 """
 For our scripts to work, we need to download the Netflix prize data. As this file is ~500 MB and the licensing terms are dubious, we decided early on not to include it as part of the repo. It is however available on the Internet Archive here: https://archive.org/details/nf_prize_dataset.tar
@@ -15,3 +18,50 @@ We can use the tarfile library to extract the files: https://docs.python.org/3/l
 
 Finally, renaming can be done with os or shutil: https://docs.python.org/3/library/os.html#os.rename
 """
+
+
+def download_file(url, compressed_filename, data_dir):
+    os.makedirs(data_dir, exist_ok=True)  # create directory if not already existing
+    output_path = os.path.join(data_dir, compressed_filename)
+
+    if os.path.exists(output_path):
+        print(f"File already exists: {output_path}")
+        return None
+    else:
+        filename = wget.download(url, out=output_path)
+        return filename
+
+
+def extract_file(compressed_filename, data_dir):
+    compressed_file_path = os.path.join(data_dir, compressed_filename)
+
+    if os.path.exists(download_dir):
+        print(f"Directory '{download_dir}' already exists. Skipping extraction.")
+    else:
+        print("Extracting...")
+        with tarfile.open(compressed_file_path, "r:gz") as tar:
+            tar.extractall(path=data_dir, filter="data")
+
+
+def move_files(data_dir, download_dir, filename):
+    target_file = os.path.join(download_dir, filename)
+    destination = os.path.join(data_dir, filename)
+
+    if not os.path.exists(target_file):
+        print("target file does not exist. Nothing to move")
+    else:
+        os.rename(target_file, destination)
+        print(f"Moving {filename}...")
+        print("Download Complete!")
+
+
+if __name__ == "__main__":
+    url = "https://archive.org/download/nf_prize_dataset.tar/nf_prize_dataset.tar.gz"
+    compressed_filename = "nf_prize_dataset.tar.gz"
+    data_dir = os.path.join(os.path.dirname(__file__), "../data")
+    download_dir = os.path.join(data_dir, "download")
+    file_name = "movie_titles.txt"
+
+    download_file(url, compressed_filename, data_dir)
+    extract_file(compressed_filename, data_dir)
+    move_files(data_dir, download_dir, file_name)
