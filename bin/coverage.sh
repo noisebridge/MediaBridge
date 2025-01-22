c97bab3aba199f356bd279337c992344c25880eb
John Hanley <jhanley634@users.noreply.github.com>
2025-01-22 14:20:06 -0800

    Add pytest --cov

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -5,10 +5,13 @@
 set -e
 source bin/util.sh
 
+ENV="env PYTHONPATH=.:../..:${PYTHONPATH}"
+
 coverage erase
 coverage run -p mediabridge/definitions.py 2> /dev/null || true
 coverage run -p -m mediabridge.definitions
 coverage run -p -m unittest mediabridge/*/*_test.py
+${ENV} pytest --cov --cov-report=term-missing
 coverage combine --quiet
 coverage html
 coverage report
