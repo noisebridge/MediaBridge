a9cb2f8548a6bbb6135ffcceba416a8c171ca828
John Hanley <jhanley634@users.noreply.github.com>
2025-01-22 10:26:40 -0800

    Exercise unused parts of the definitions module

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -6,6 +6,9 @@ set -e
 source bin/util.sh
 
 coverage erase
-coverage run -m unittest mediabridge/*/*_test.py
+coverage run -p mediabridge/definitions.py 2> /dev/null || true
+coverage run -p -m mediabridge.definitions
+coverage run -p -m unittest mediabridge/*/*_test.py
+coverage combine --quiet
 coverage html
 coverage report
