0b8f02c8502d9c7630cebc13025780b42d39fadd
John Hanley <jhanley634@users.noreply.github.com>
2025-01-22 16:29:13 -0800

    Add test_create_netflix_csv(), while dealing with "year,title" versus "title,year"

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -8,7 +8,7 @@ source bin/util.sh
 ENV="env PYTHONPATH=.:../..:${PYTHONPATH}"
 
 coverage erase
-${ENV} coverage run -p -m pytest
+${ENV} coverage run -p -m pytest -v
 coverage run -p mediabridge/definitions.py 2> /dev/null || true
 coverage run -p -m mediabridge.definitions
 coverage run -p -m unittest mediabridge/*/*_test.py
--- mediabridge/data_processing/wiki_to_netflix_test.py
+++ mediabridge/data_processing/wiki_to_netflix_test.py
@@ -5,6 +5,8 @@ from mediabridge.data_processing.wiki_to_netflix_test_data import EXPECTED_SPARQ
 from mediabridge.definitions import DATA_DIR
 from mediabridge.schemas.movies import EnrichedMovieData, MovieData
 
+TITLES_TXT = DATA_DIR / "movie_titles.txt"
+
 
 class TestWikiToNetflix(unittest.TestCase):
     def test_format_sparql_query(self) -> None:
@@ -33,12 +35,22 @@ class TestWikiToNetflix(unittest.TestCase):
         )
 
     def test_read_netflix_txt(self) -> None:
-        titles_txt = DATA_DIR / "movie_titles.txt"
-
-        movies = list(w_to_n.read_netflix_txt(titles_txt))
+        movies = list(w_to_n.read_netflix_txt(TITLES_TXT))
         assert len(movies) == 17_770
         assert movies[-1] == ["17770", "2003", "Alien Hunter"]
 
-        movies = list(w_to_n.read_netflix_txt(titles_txt, 3))
+        movies = list(w_to_n.read_netflix_txt(TITLES_TXT, 3))
         assert len(movies) == 3
         assert movies[-1] == ["3", "1997", "Character"]
+
+    def test_create_netflix_csv(self) -> None:
+        output_csv = DATA_DIR / "movie_titles.csv"
+        few_rows = 3  # A conveniently small number of rows, for fast tests.
+        rows = list(w_to_n.read_netflix_txt(TITLES_TXT, few_rows))
+        movies = [MovieData(id, title, int(year)) for id, year, title in rows]
+
+        w_to_n.create_netflix_csv(output_csv, movies)
+        lines = output_csv.read_text().splitlines()
+
+        assert len(lines) == few_rows + 1
+        assert lines[-1] == "3,Character,1997"
