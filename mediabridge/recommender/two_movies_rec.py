a64b527604a8ef7ad4527d39e99f7b3d3cb4845a
John Hanley <jhanley634@users.noreply.github.com>
2025-01-30 21:16:09 -0800

    Deal with "unknown year"

--- mediabridge/recommender/two_movies_rec.py
+++ mediabridge/recommender/two_movies_rec.py
@@ -2,6 +2,7 @@
 Recommends movies based on sparse input: the "cold start" problem.
 """
 
+import numpy as np
 import pandas as pd
 
 from mediabridge.data_processing.wiki_to_netflix import read_netflix_txt
@@ -9,5 +10,8 @@ from mediabridge.definitions import FULL_TITLES_TXT
 
 
 def etl() -> None:
-    netflix_df = pd.DataFrame(read_netflix_txt(FULL_TITLES_TXT))
-    print(netflix_df)
+    df = pd.DataFrame(
+        read_netflix_txt(FULL_TITLES_TXT), columns=["id", "year", "title"]
+    )
+    df["year"] = df.year.replace("NULL", pd.NA).astype("Int16")
+    print(df)
