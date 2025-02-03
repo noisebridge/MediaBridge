84cfbeca7eb4e495547a7cc0574a941b1cb0a67b
John Hanley <jhanley634@users.noreply.github.com>
2025-02-02 17:59:26 -0800

    Avoid ETL of movie titles if they're not available, e.g. during a CI run

--- tests/recommender/two_movies_rec_test.py
+++ tests/recommender/two_movies_rec_test.py
@@ -4,6 +4,7 @@ from warnings import catch_warnings, filterwarnings
 import pandas as pd
 
 from mediabridge.db.tables import create_tables
+from mediabridge.definitions import FULL_TITLES_TXT
 from mediabridge.recommender.two_movies_rec import etl
 
 
@@ -16,7 +17,8 @@ class TestTwoMoviesRec(unittest.TestCase):
 
             assert LightFM(loss="warp")
             create_tables()
-            etl()
+            if FULL_TITLES_TXT.exists():
+                etl()
 
     def test_df(self) -> None:
         df = pd.DataFrame()
