df7741c09dfcc5a1d6247179c402756a47502c03
John Hanley <jhanley634@users.noreply.github.com>
2025-02-06 11:20:10 -0800

    Add _get_ratings()

--- mediabridge/recommender/two_movies_rec.py
+++ mediabridge/recommender/two_movies_rec.py
@@ -2,9 +2,78 @@
 Recommends movies based on sparse input: the "cold start" problem.
 """
 
-from lightfm import LightFM
+from warnings import filterwarnings
+
+import numpy as np
+from scipy.sparse import coo_matrix, dok_matrix
+from sqlalchemy import text
+
+from mediabridge.db.tables import get_engine
 
 
-def recommend() -> None:
+def recommend(
+    max_training_user_id: int = 2_000,
+) -> None:
+    message = "LightFM was compiled without OpenMP support"
+    filterwarnings("ignore", message, category=UserWarning)
+    from lightfm import LightFM
+
     model = LightFM(no_components=30)
-    print("\n", model)
+    train = _get_ratings(max_training_user_id)
+    model.fit(train, epochs=10, num_threads=4)
+
+    test_user_ids, test_movie_ids = _get_test_data(max_training_user_id)
+
+    predictions = model.predict(test_user_ids, test_movie_ids)
+    print(predictions)
+
+
+def _get_ratings(
+    max_user_id: int,
+    threshold: int = 4,
+) -> coo_matrix:
+    """Produces a sparse training matrix of just "positive" user ratings.
+
+    We do not yet return any "negative" -1 values, e.g. for two- and one- star reviews.
+    """
+    query = """
+    SELECT
+        user_id,
+        CAST(movie_id AS INTEGER)
+    FROM rating
+    WHERE
+        user_id <= :max_user_id
+        AND rating >= :threshold
+    ORDER BY user_id, movie_id
+    """
+    params = {
+        "max_user_id": max_user_id,
+        "threshold": threshold,
+    }
+    matrix = dok_matrix(
+        (max_user_id + 1, _get_max_movie_id() + 1),
+        dtype=np.int8,
+    )
+    with get_engine().connect() as conn:
+        for u, m in conn.execute(text(query), params):
+            matrix[u, m] = 1
+    return coo_matrix(matrix)
+
+
+def _get_max_movie_id() -> int:
+    # Movie IDs are VARCHARs in the database, which complicates matters.
+    # That magic number of 4 is a pretty terrible to return 17_770 instead of 9999.
+    query = "SELECT MAX(id)  FROM movie_title  WHERE LENGTH(id) > 4"
+    with get_engine().connect() as conn:
+        val = conn.execute(text(query)).scalar()
+        return int(val or 0)
+
+
+def _get_test_data(cutoff_user_id: int) -> tuple[list[int], list[int]]:
+    query = "SELECT DISTINCT user_id  FROM rating  WHERE user_id > :cutoff_user_id  ORDER BY user_id"
+    params = {"cutoff_user_id": cutoff_user_id}
+    with get_engine().connect() as conn:
+        test_user_ids = [u for u, in conn.execute(text(query), params)]
+        query = "SELECT DISTINCT id  FROM movie_title"
+        test_movie_ids = [int(m) for m, in conn.execute(text(query))]
+        return test_user_ids[:17_770], sorted(test_movie_ids)
--- tests/recommender/two_movies_rec_test.py
+++ tests/recommender/two_movies_rec_test.py
@@ -21,11 +21,16 @@ class TestTwoMoviesRec(unittest.TestCase):
         # We might choose another way to run the ETL, outside of a test framework.
         # The current code was enough to support an edit-run cycle during initial development.
 
+        # For example, tests may run in arbitrary order, yet recommend() depends on ETL.
+        # ETL effects persist across test runs, so a failed initial run will let later tests succeed.
+
         create_tables()
         if FULL_TITLES_TXT.exists():
             # A million rows corresponds to a four-second ETL.
             t0 = time()
+
             etl("mv_00*.txt", max_rows=1_000_000)
+
             elapsed = time() - t0
             x = (elapsed < 10) or print(f"ETL finished in {elapsed:.3f} s")
             assert x in (True, None)  # we simply evaluated for side effects
