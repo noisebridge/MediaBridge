88ee791ac39ff2fb1a0c474beb301450e99ea5de
John Hanley <jhanley634@users.noreply.github.com>
2025-02-06 12:18:12 -0800

    Add _get_test_movie_ids()

--- mediabridge/recommender/two_movies_rec.py
+++ mediabridge/recommender/two_movies_rec.py
@@ -12,24 +12,34 @@ from mediabridge.db.tables import get_engine
 
 
 def recommend(
-    max_training_user_id: int = 2_000,
+    max_training_user_id: int = 800,
+    large_movie_id: int = 9_770,
 ) -> None:
     message = "LightFM was compiled without OpenMP support"
     filterwarnings("ignore", message, category=UserWarning)
     from lightfm import LightFM
 
     model = LightFM(no_components=30)
-    train = _get_ratings(max_training_user_id)
+    train = _get_ratings(max_training_user_id, large_movie_id)
     model.fit(train, epochs=10, num_threads=4)
 
-    test_user_ids, test_movie_ids = _get_test_data(max_training_user_id)
+    test_movie_ids = _get_test_movie_ids(large_movie_id)
 
-    predictions = model.predict(test_user_ids, test_movie_ids)
-    print(predictions)
+    predictions = model.predict(max_training_user_id, test_movie_ids)
+    assert isinstance(predictions, np.ndarray)
+    assert predictions.shape == (len(test_movie_ids),)  # (8000, )
+    mx = round(predictions.max(), 5)
+    assert 0.0016 < mx < 0.0022, mx
+    thresh = 0.85 * mx  # admit some more recommendations
+    for i, p in enumerate(predictions):
+        if p > thresh:
+            netflix_id = test_movie_ids[i]  # map from internal to external ID
+            print(i, "  ", netflix_id, "\t", p, "\t", _get_title(netflix_id))
 
 
 def _get_ratings(
     max_user_id: int,
+    large_movie_id: int,
     threshold: int = 4,
 ) -> coo_matrix:
     """Produces a sparse training matrix of just "positive" user ratings.
@@ -57,23 +67,46 @@ def _get_ratings(
     with get_engine().connect() as conn:
         for u, m in conn.execute(text(query), params):
             matrix[u, m] = 1
+
+            # Blind the model to the movies we want to predict.
+            if u == max_user_id and m >= large_movie_id:
+                matrix[u, m] = 0
+                del matrix[u, m]
+                print(u, m)
+
     return coo_matrix(matrix)
 
 
 def _get_max_movie_id() -> int:
     # Movie IDs are VARCHARs in the database, which complicates matters.
-    # That magic number of 4 is a pretty terrible to return 17_770 instead of 9999.
+    # That horrible magic number of 4 lets us return 17_770 instead of 9999.
     query = "SELECT MAX(id)  FROM movie_title  WHERE LENGTH(id) > 4"
     with get_engine().connect() as conn:
         val = conn.execute(text(query)).scalar()
         return int(val or 0)
 
 
-def _get_test_data(cutoff_user_id: int) -> tuple[list[int], list[int]]:
-    query = "SELECT DISTINCT user_id  FROM rating  WHERE user_id > :cutoff_user_id  ORDER BY user_id"
-    params = {"cutoff_user_id": cutoff_user_id}
+def _get_test_movie_ids(
+    large_movie_id: int,
+) -> list[int]:
+    query = """
+    SELECT id
+    FROM movie_title
+    WHERE CAST(id AS INTEGER) > :max_training_user_id
+    """
+    params = {"max_training_user_id": large_movie_id}
     with get_engine().connect() as conn:
-        test_user_ids = [u for u, in conn.execute(text(query), params)]
-        query = "SELECT DISTINCT id  FROM movie_title"
-        test_movie_ids = [int(m) for m, in conn.execute(text(query))]
-        return test_user_ids[:17_770], sorted(test_movie_ids)
+        test_movie_ids = [int(m) for m, in conn.execute(text(query), params)]
+        return sorted(test_movie_ids)
+
+
+def _get_title(netflix_id: int) -> str:
+    query = """
+    SELECT title
+    FROM movie_title
+    WHERE CAST(id AS INTEGER) = :netflix_id
+    """
+    params = {"netflix_id": netflix_id}
+    with get_engine().connect() as conn:
+        title = conn.execute(text(query), params).scalar()
+        return str(title)
