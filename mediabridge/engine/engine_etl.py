cbee7554d250a38ed6cf5a3d4be8ca3c5ffe73a1
John Hanley <jhanley634@users.noreply.github.com>
2025-02-15 12:16:50 -0800

    Take care to close() connection

--- mediabridge/engine/engine_etl.py
+++ mediabridge/engine/engine_etl.py
@@ -6,13 +6,14 @@ from mediabridge.definitions import FULL_TITLES_TXT
 
 
 def etl_mongo_movie_titles() -> None:  # pragma: no cover
-    df = pd.read_csv(DATA_DIR / "movie_titles.csv")
-    df["netflix_id"] = df.netflix_id.astype(str)
+    columns = ["netflix_id", "year", "title"]
+    df = pd.DataFrame(read_netflix_txt(FULL_TITLES_TXT), columns=columns)
     rows = df.to_dict(orient="records")
     print(f"{len(rows)} rows ", end="", flush=True)
 
-    client = connect_to_mongo()
-    movies_collection = client["movies"]
+    db = connect_to_mongo()
+    movies_collection = db["movies"]
 
     movies_collection.insert_many(rows)
+    db.client.close()
     print("inserted")
