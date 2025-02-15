2547880ea7b495cf049af74d24716aacecf6288b
John Hanley <jhanley634@users.noreply.github.com>
2025-02-15 11:21:34 -0800

    Mark that etl_mongo_movie_titles is not expected to execute on every interactive test run

--- mediabridge/engine/engine_etl.py
+++ mediabridge/engine/engine_etl.py
@@ -4,7 +4,7 @@ from mediabridge.db.connect import connect_to_mongo
 from mediabridge.definitions import DATA_DIR
 
 
-def etl_mongo_movie_titles() -> None:
+def etl_mongo_movie_titles() -> None:  # pragma: no cover
     df = pd.read_csv(DATA_DIR / "movie_titles.csv")
     df["netflix_id"] = df.netflix_id.astype(str)
     rows = df.to_dict(orient="records")
