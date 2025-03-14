3b7f13354ebc7e95829562cd5e84a59e42bc18ac
John Hanley <jhanley634@users.noreply.github.com>
2025-03-14 11:42:24 -0700

    Add test_sqlite_dot_import()

---
+++ tests/data_processing/etl_test.py
@@ -0,0 +1,31 @@
+import unittest
+from pathlib import Path
+
+import pandas as pd
+
+from mediabridge.data_processing.etl import _run_sqlite_child
+
+
+class EtlTest(unittest.TestCase):
+    def test_run_sqlite_child(self) -> None:
+        _run_sqlite_child(
+            [
+                "DROP TABLE  IF EXISTS  temp_table;",
+                "CREATE TABLE temp_table AS SELECT 123 AS num, 'abc' AS alpha;",
+                "DROP TABLE temp_table;",
+            ]
+        )
+
+    def test_sqlite_dot_import(self) -> None:
+        foo = pd.DataFrame([{"num": 123, "alpha": "abc"}])
+        csv = Path("/tmp/foo.csv")
+        foo.to_csv(csv, index=False)
+        _run_sqlite_child(
+            [
+                "DROP TABLE  IF EXISTS  foo;",
+                ".mode csv",
+                ".headers off",
+                f".import {csv} foo",
+            ]
+        )
+        csv.unlink()
