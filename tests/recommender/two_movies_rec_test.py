613af0be83fd1f8ad298409acd9fd9439bf57325
John Hanley <jhanley634@users.noreply.github.com>
2025-03-14 16:18:56 -0700

    110 write a failing test (#112)
    * Add test_sqlite_dot_import()
    
    * Expose run_sqlite_child() as a public symbol
    
    * Demonstrate contrasting behavior of .import when table already {does, does not} exist
    
    * Delete the create_rating_csv DDL -- not only is it not needed, but it alters the behavior of .import, causing the header row to be INSERTed like a data row
    
    * Quote an excerpt from the documentation
    
    * Add _validate_rating_table()
    
    * Unrelated item: delete no longer needed test_etl(), as tables are now created by "mb load"
    
    * Remove unused imports
    
    * Call create_tables(), in case we're running in CI/CD and we have no tables yet
    
    * Put diagnostic message after an assert

--- tests/recommender/two_movies_rec_test.py
+++ tests/recommender/two_movies_rec_test.py
@@ -1,8 +1,5 @@
 import unittest
-from time import time
 
-from mediabridge.data_processing.etl import etl
-from mediabridge.db.tables import create_tables
 from mediabridge.definitions import TITLES_TXT
 from mediabridge.recommender.make_recommendation import get_title, recommend
 
@@ -23,24 +20,6 @@ def _clean(ids: set[int]) -> set[int]:
 class TestTwoMoviesRec(unittest.TestCase):
     # Ideally we should be able to have a user supply two movies they like,
     # and recommend a few more to them. We're not quite there yet.
-
-    def test_etl(self) -> None:
-        # We might choose another way to run the ETL, outside of a test framework.
-        # The current code was enough to support an edit-run cycle during initial development.
-
-        # For example, tests may run in arbitrary order, yet recommend() depends on ETL.
-        # ETL effects persist across test runs, so a failed initial run will let later tests succeed.
-
-        create_tables()
-        if TITLES_TXT.exists():
-            # A million rows corresponds to a four-second ETL.
-            t0 = time()
-
-            etl(max_reviews=1_000_000, regen=True)
-
-            elapsed = time() - t0
-            x = (elapsed < 10) or print(f"ETL finished in {elapsed:.3f} s")
-            assert x in (True, None)  # we simply evaluated for side effects
 
     def test_recommend(self) -> None:
         if TITLES_TXT.exists():
