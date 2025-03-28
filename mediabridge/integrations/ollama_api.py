c486a637b9ec70b82982e136ac9f35f917e89580
JamesKohlsRepo <jameskohls@live.com>
2025-03-28 09:37:51 -0700

    Added Nginx Reverse proxy for authentication
    Implemented basis security for Ollama API. Ollama does not come with authentication, but they documented how to use ngix with ollama.

--- mediabridge/integrations/ollama_api.py
+++ mediabridge/integrations/ollama_api.py
@@ -1,9 +1,12 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
 """
 run main with:
 PYTHONPATH=$(pwd) pipenv run python mediabridge/integrations/ollama_api.py
 """
+
+AUTH = HTTPBasicAuth("ollamauser", "INPUT_PASSWORD_HERE")
 
 
 def get_headers() -> dict:
@@ -14,8 +17,8 @@ def get_headers() -> dict:
 def generate_prompt_response(model: str, prompt: str, url: str) -> str:
     """Sends a prompt to the Ollama API and returns the response as plain text"""
     payload = {"model": model, "prompt": prompt, "stream": False}
-    response = requests.post(url, headers=get_headers(), json=payload)
-    response.raise_for_status()  # raise an error if request fails
+    response = requests.post(url, headers=get_headers(), auth=AUTH, json=payload)
+    response.raise_for_status()
     response_json = response.json()
     return response_json.get("response", "")
 
@@ -24,7 +27,7 @@ if __name__ == "__main__":
     api_base_url = "https://ollama.tomato-pepper.uk/api"
 
     tags_url = f"{api_base_url}/tags"
-    response = requests.get(tags_url, headers=get_headers())
+    response = requests.get(tags_url, headers=get_headers(), auth=AUTH)
     models_data = response.json().get("models", [])
 
     print("Available models:")
