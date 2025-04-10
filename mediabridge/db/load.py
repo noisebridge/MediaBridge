d5484a68bb653e58890cf32dfa2233ad3a1c1371
Travis Briggs <audiodude@gmail.com>
2025-04-10 09:49:13 -0700

    Load movie data into mongo based on sqlite database

--- mediabridge/db/load.py
+++ mediabridge/db/load.py
@@ -1,9 +1,14 @@
 import csv
 import dataclasses
 import logging
+from typing import List
 
+from pymongo import InsertOne
+from sqlalchemy import text
 from typer import Typer
 
+from mediabridge.db.connect import connect_to_mongo
+from mediabridge.db.tables import get_engine
 from mediabridge.definitions import OUTPUT_DIR
 from mediabridge.schemas.movies import EnrichedMovieData
 
@@ -37,6 +42,31 @@ def load() -> None:
             )
             log.info(f"Inserting {movie} into MongoDB")
             # TODO: Needs implementation, bulk inserts for performance
+
+
+def _make_movie_dict(movie: List) -> dict:
+    return {
+        "netflix_id": movie[0],
+        "title": movie[1],
+        "year": movie[2],
+    }
+
+
+def load_from_sql(regen: bool) -> None:
+    """Load movies into the movies collection based on data in the sqlite database.
+
+    Args:
+        regen (bool): If True, the movies collection will be dropped and recreated.
+    """
+    mongo_movies = connect_to_mongo().movies
+    if regen:
+        mongo_movies.delete_many({})
+    with get_engine().connect() as conn:
+        operations = [
+            InsertOne(_make_movie_dict(movie))
+            for movie in conn.execute(text("SELECT id, title, year FROM movie_title"))
+        ]
+        mongo_movies.bulk_write(operations)
 
 
 if __name__ == "__main__":
