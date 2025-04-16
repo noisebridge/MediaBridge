8fa06fdce17a46a381657e1dc8b57811da055ffb
JamesKohlsRepo <jameskohls@live.com>
2025-04-15 23:01:02 -0700

    removed placeholder mongo test

--- tests/data_processing/mongo_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-""" run with: PYTHONPATH=$(pwd) pipenv run python -m unittest tests/data_processing/mongo_test.py """
-
-
-class TestMongo(unittest.TestCase):
-    def test_placeholder(self):
-        """A placeholder test that always passes."""
-        self.assertTrue(True)
