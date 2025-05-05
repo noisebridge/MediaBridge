659ad20242e47a80436245c42e2e133c8477eac0
John Hanley <jhanley634@users.noreply.github.com>
2025-05-05 13:50:26 -0700

    Un-nest the app functions, so e.g. search_movies() is publicly visible

--- mediabridge/api/main.py
+++ mediabridge/api/main.py
@@ -8,32 +8,31 @@ from mediabridge.db.tables import get_engine
 typer_app = typer.Typer()
 
 
-def create_app():
-    app = flask.Flask(__name__)
-    CORS(app)
+app = flask.Flask(__name__)
+CORS(app)
 
-    @app.route("/")
-    def hello_world():
-        return "MediaBridge API is running!"
 
-    @app.route("/api/v1/movie/search")
-    def search_movies():
-        query = flask.request.args.get("q")
-        if not query:
-            return flask.jsonify({"error": "Query parameter 'q' is required."}), 400
+@app.route("/")
+def hello_world():
+    return "MediaBridge API is running!"
 
-        with get_engine().connect() as conn:
-            pattern = f"%{query}%"
-            movies = conn.execute(
-                text(
-                    "SELECT * FROM movie_title WHERE LOWER(title) LIKE LOWER(:pattern) LIMIT 10"
-                ),
-                {"pattern": pattern},
-            ).fetchall()
-            movies_list = [row._asdict() for row in movies]
-            return flask.jsonify(movies_list)
 
-    return app
+@app.route("/api/v1/movie/search")
+def search_movies():
+    query = flask.request.args.get("q")
+    if not query:
+        return flask.jsonify({"error": "Query parameter 'q' is required."}), 400
+
+    with get_engine().connect() as conn:
+        pattern = f"%{query}%"
+        movies = conn.execute(
+            text(
+                "SELECT * FROM movie_title WHERE LOWER(title) LIKE LOWER(:pattern) LIMIT 10"
+            ),
+            {"pattern": pattern},
+        ).fetchall()
+        movies_list = [row._asdict() for row in movies]
+        return flask.jsonify(movies_list)
 
 
 @typer_app.command()
@@ -41,5 +40,4 @@ def serve(ctx: typer.Context, debug: bool = True):
     """
     Serve the Flask app.
     """
-    app = create_app()
     app.run(debug=debug)
