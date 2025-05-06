5519605bb1cee12df6707bac4e8e04e37e4ae76d
John Hanley <jhanley634@users.noreply.github.com>
2025-05-05 17:26:22 -0700

    Restore api/main.py, per request of Travis

--- mediabridge/api/main.py
+++ mediabridge/api/main.py
@@ -1,5 +1,5 @@
+import flask
 import typer
-from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 from sqlalchemy import text
 
@@ -8,35 +8,38 @@ from mediabridge.db.tables import get_engine
 typer_app = typer.Typer()
 
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = flask.Flask(__name__)
+    CORS(app)
 
+    @app.route("/")
+    def hello_world():
+        return "MediaBridge API is running!"
 
-@app.route("/")  # type: ignore
-def hello_world() -> str:
-    return "MediaBridge API is running!"
+    @app.route("/api/v1/movie/search")
+    def search_movies():
+        query = flask.request.args.get("q")
+        if not query:
+            return flask.jsonify({"error": "Query parameter 'q' is required."}), 400
 
+        with get_engine().connect() as conn:
+            pattern = f"%{query}%"
+            movies = conn.execute(
+                text(
+                    "SELECT * FROM movie_title WHERE LOWER(title) LIKE LOWER(:pattern) LIMIT 10"
+                ),
+                {"pattern": pattern},
+            ).fetchall()
+            movies_list = [row._asdict() for row in movies]
+            return flask.jsonify(movies_list)
 
-@app.route("/api/v1/movie/search")  # type: ignore
-def search_movies() -> tuple[Response, int]:
-    query = request.args.get("q")
-    if not query:
-        return jsonify({"error": "Query parameter 'q' is required."}), 400
-
-    with get_engine().connect() as conn:
-        select = "SELECT id, year, title FROM movie_title WHERE LOWER(title) LIKE LOWER(:pattern) LIMIT 10"
-        pattern = f"%{query}%"
-        movies = conn.execute(
-            text(select),
-            {"pattern": pattern},
-        ).fetchall()
-        movies_list = [row._asdict() for row in movies]
-        return jsonify(movies_list), 200
+    return app
 
 
 @typer_app.command()
-def serve(ctx: typer.Context, debug: bool = True) -> None:
+def serve(ctx: typer.Context, debug: bool = True):
     """
     Serve the Flask app.
     """
+    app = create_app()
     app.run(debug=debug)
